7c1874282943e96d1345881ac682a3c449d5a5fe
Rahul <rahul.gupta@kelltontech.com>
2022-07-16 21:34:46 +0530

    deploye on heroku

---
+++ portfolio/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from . forms import NoteForm
+from .models import Note
+from django.views.decorators.csrf import csrf_protect
+# Create your views here.
+def home(request):
+    return render(request,"portfolio/Home.html")
+
+@csrf_protect
+def contacus(request):
+
+    form=NoteForm()
+    data_retrieve_from_noteform=Note.objects.filter()
+    data={
+        "form":form,
+        "data_retrieve_frm_database":data_retrieve_from_noteform
+    }
+    
+    if request.method=="POST":
+        
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        subject=request.POST.get('subject')
+        description=request.POST.get('description')
+        
+        note=Note()
+        note.name=name
+        note.email=email
+        note.description=description
+        note.subject=subject
+        note.save()
+        print("Successfully save in database")
+    return render(request,"portfolio/contact.html",context=data)
---
+++ service/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def experience(request):
+    return render(request,'service/exp.html')
