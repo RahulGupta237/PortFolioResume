fb48a868d8c6815c927b6eafdd8ec9bd8f00bf4d
Rahul <rahul.gupta@kelltontech.com>
2022-07-17 00:46:38 +0530

    2nd commite

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT resume_project.wsgi:application
--- resume_project/settings.py
+++ resume_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)i%ji#!31x%$sp0##215s5+t+852oeuv#cn(1jo)s4%%4&fcc0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["heroku.com/images237","localhost","*","8001"]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,9 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = 'static/'
-STATIC_DIR=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=BASE_DIR/'portfolio/static'
+STATIC_DIR=os.path.join(BASE_DIR,'portfolio/static')
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
